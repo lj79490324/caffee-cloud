32d111a2e6fa30e8e37fa82c157375f49267a395
Rabit <lj79490324@gmail.com>
2023-04-07 15:34:56 +0800

    test(coffee-common-system): 标准化注释更新测试

--- coffee-modules/coffee-system/src/main/java/com/coffee/system/dto/SysRoleMenuDto.java
+++ coffee-modules/coffee-system/src/main/java/com/coffee/system/dto/SysRoleMenuDto.java
@@ -1,11 +1,19 @@
 package com.coffee.system.dto;
 
 import com.coffee.system.model.SysRoleMenu;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+/**
+ * 授权的dto 帮助返回菜单名称和属性菜单
+ * @author rabit
+ */
 @Data
 public class SysRoleMenuDto extends SysRoleMenu {
+    @Schema(description = "父级菜单id")
     private Long parentId;
+    @Schema(description = "菜单名称")
     private String sysMenuName;
+    @Schema(description = "菜单icon")
     private String sysMenuIcon;
 }
