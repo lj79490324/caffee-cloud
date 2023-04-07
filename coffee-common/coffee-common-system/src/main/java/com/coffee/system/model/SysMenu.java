d736c1b788905407927df432713c364d139540f4
Rabit <lj79490324@gmail.com>
2023-04-07 15:17:49 +0800

    docs(coffee-common-mysql): 标准化注释更新

--- coffee-common/coffee-common-system/src/main/java/com/coffee/system/model/SysMenu.java
+++ coffee-common/coffee-common-system/src/main/java/com/coffee/system/model/SysMenu.java
@@ -6,16 +6,24 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+/**
+ * 权限菜单表
+ * @author rabit
+ */
 @Data
 public class SysMenu implements Serializable {
+    @Schema(description = "菜单id",minimum = "1",requiredMode = Schema.RequiredMode.AUTO)
     @TableId(type = IdType.AUTO)
     private Long id;
+    @Schema(description = "父级菜单id",minimum = "1",requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private Long parentId;
+    @Schema(description = "菜单名称",minimum = "1",requiredMode = Schema.RequiredMode.REQUIRED)
     private String sysMenuName;
     private String sysMenuCode;
     private String sysMenuUrl;
--- coffee-common/coffee-datasource-mysql/src/main/java/com/coffee/common/mysql/configure/CoffeeMetaObjectHandler.java
+++ coffee-common/coffee-datasource-mysql/src/main/java/com/coffee/common/mysql/configure/CoffeeMetaObjectHandler.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+/**
+ * mp配置自动填充功能插件
+ * 参考：https://baomidou.com/pages/4c6bcf/
+ * @author rabit
+ */
 @Component
 public class CoffeeMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class); // 起始版本 3.3.3(推荐)
+        // 起始版本 3.3.3(推荐)
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
     }
 
     @Override
